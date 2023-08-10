27ae3e593575d805710905a282c969aa2f35467a
Garrett <garrettswink421@gmail.com>
2023-08-10 17:27:19 -0400

    initial app setup

---
+++ index.js
@@ -0,0 +1,27 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const questions = require('./lib/questions.js');
+const fileName = './examples/logo.svg';
+const newShape = require('./lib/newShape.js');
+
+
+function generateLogo() {
+    inquirer.prompt(questions)
+        .then(response => {
+            let svg = newShape(response);
+
+            fs.writeFile(fileName, svg, (err) => {
+                if (err) {
+                    console.log('Error writing file', err);
+                } else {
+                    console.log('File written successfully');
+                }
+            });
+        })
+        .catch(err => {
+            console.errer('Error during inquiry:', err);
+        });
+};
+
+generateLogo();
